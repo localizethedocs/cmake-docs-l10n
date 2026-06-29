2b899fec82d6b027c51a63ca6d29f9d466e11f09
ltdrobot <ltdrobot@localizethedocs.org>
2026-06-29 14:00:01 +0000

    Deploying to pages from @ localizethedocs/cmake-docs-l10n@2acb92552a90cb0ffa4b5bf953322418cc39147d 🚀

---
+++ ltd-config.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["ru-ru",   "Русский"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["master",  "Development"],
+    ["latest",  "Latest Release"],
+    ["4",       "Release 4.x"],
+    ["3",       "Release 3.x"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/cmake-docs-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/cmake-docs-l10n"],
+    ["GitCode", "https://gitcode.com/localizethedocs/cmake-docs-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/cmake-docs-l10n"],
+  ]
+};
