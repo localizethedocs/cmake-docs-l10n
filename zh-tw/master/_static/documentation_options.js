d647c58ac0b21e2fc6d806706e43b135ceded71f
ltdrobot <ltdrobot@localizethedocs.org>
2026-08-31 17:30:30 +0000

    Deploying to pages from @ localizethedocs/cmake-docs-l10n@d8ac0972ef25302de744b4ac3313a106db7d7309 🚀

---
+++ zh-tw/master/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '4.4.20260622-gee0198b',
+    LANGUAGE: 'zh-TW',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
